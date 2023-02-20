809772598aaef4d6586339e1730f57f7496fb77c
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-02-20 10:18:49 +0530

    Added: program to find max String among three numbers

--- MaximumAmongThree.java
+++ MaximumAmongThree.java
@@ -1,16 +1,16 @@
 package com.maximum;
 public class MaximumAmongThree {
-    public static <Float extends Comparable<Float>> Float maximumFloat(Float number1, Float number2, Float number3) {
-        Float maximumNumber = number1;
-        if (number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
-            maximumNumber = number1;
-        else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
-            maximumNumber = number2;
+    public static <String extends Comparable<String>> String maximumString(String name1, String name2, String name3) {
+        String maximumNumber = name1;
+        if (name1.compareTo(name2) > 0 && name1.compareTo(name3) > 0)
+            maximumNumber = name1;
+        else if (name2.compareTo(name1) > 0 && name2.compareTo(name3) > 0)
+            maximumNumber = name2;
         else
-            maximumNumber = number3;
+            maximumNumber = name3;
         return maximumNumber;
     }
     public static void main(String[] args) {
-        System.out.println("Maximum Number among 3 will be: " + maximumFloat(18.5f, 7.7f, 9.8f));
+        System.out.println("Maximum Number among 3 will be: " + maximumString("ramesh", "virat", "kohli"));
     }
 }
