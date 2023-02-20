055bc7ac161a15b459341ad6b5375066e1de3c0d
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-02-20 10:05:05 +0530

    Added: program to find max integer among three numbers

--- MaximumAmongThree.java
+++ MaximumAmongThree.java
@@ -1,9 +1,7 @@
 package com.maximum;
-
-public class MaximumAmongThree<T> {
-    T number1, number2, number3;
-    public static <T extends Comparable<T>> T maximumAmongThree(T number1, T number2, T number3) {
-        T maximumNumber = number1;
+public class MaximumAmongThree {
+    public static <Integer extends Comparable<Integer>> Integer maximumInteger(Integer number1, Integer number2, Integer number3) {
+        Integer maximumNumber = number1;
         if (number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
             maximumNumber = number1;
         else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
@@ -13,9 +11,6 @@ public class MaximumAmongThree<T> {
         return maximumNumber;
     }
     public static void main(String[] args) {
-        System.out.println("The max Number Among Three String numbers is:-\n " + maximumAmongThree("virat", "ABD", "rohit"));
-        System.out.println("The max Number Among Three Integer numbers is:-\n " + maximumAmongThree(18, 17, 45));
-        System.out.println("The max Number Among Three Float numbers is:-\n " + maximumAmongThree(18.0f, 17.5f, 45.6f));
+        System.out.println("Maximum Number among 3 will be: " + maximumInteger(18, 7, 9));
     }
 }
-
